66ea4a7c266b9162ea07bb10fa5b4f6cb6ddc5f1
Carlos Eduardo Marques Barreto <carloseduardomarquesbarretom@gmail.com>
2024-08-10 21:53:22 -0300

    Update script.js

---
+++ Interação-Javascript/Modulo 1-Introdução/ExplorandoTiposDados_Número_03/script.js
@@ -0,0 +1,40 @@
+// Como criar uma variável  
+let string = "Ola fulano de tal";
+
+// Exemplos
+let mensagem = "Olá, ";
+let mensagem2 = "seguidor";
+let pessoa = "Jonathan";
+
+let mensagem3 = `${pessoa}, Seja Bem Vindo !`;
+
+let concatenacao = mensagem + mensagem2;
+//console.log(concatenacao);
+//console.log(mensagem3);
+
+// Interação Dinamica
+    //let usuario = prompt("Me diga, qual é seu nome ?");
+    //alert(`${usuario}, seja bem vindo(a) !`);
+
+// Arrays 
+
+let cargos = ["Admin","Médico","Programador","Analista","Cozinheiro"];
+cargos[1] = "Contador";
+cargos.push("Cantor","Baterista");// adicionou por ultimo
+cargos.pop();// excluí o ultimo
+cargos.shift();// excluí o primeiro
+cargos.unshift("Enfermeira");// Adiciona no começo
+//console.log(cargos[1]);
+//console.log(cargos);
+//console.log(cargos.includes("Enfermeira"))
+//console.log(cargos.indexOf("Médico"))
+//console.log(cargos.sort())
+/*
+let numerospt1 =[1,2,3,4,5];
+let numerospt2 =[6,7,8,9,10];
+ 
+const juncao =  numerospt1.concat(numerospt2);
+console.log(juncao);
+*/
+
+// Data 
