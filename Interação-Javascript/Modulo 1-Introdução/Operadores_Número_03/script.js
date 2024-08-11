40139f0db0709fddc9f78261280ae21ae49b2c44
Carlos Eduardo Marques Barreto <carloseduardomarquesbarretom@gmail.com>
2024-08-11 17:17:43 -0300

    Update script.js

--- Interação-Javascript/Modulo 1-Introdução/Operadores_Número_03/script.js
+++ Interação-Javascript/Modulo 1-Introdução/Operadores_Número_03/script.js
@@ -18,6 +18,7 @@ console.log("Exponencial:",Exponencial);
 console.log("Resto Da Divisão:",Módulo);
 */
 
+/*
 let num1 = 20;
 let num2 = 5;
 
@@ -26,3 +27,26 @@ console.log("Subtração :",num1 -= num2);
 console.log("Multiplicação :",num1 *= num2);
 console.log("Divisão :",num1 /= num2);
 console.log("Resto De Divisão :",num1 %= num2);
+*/
+
+/*
+let num1 = 15;
+let num2 = 9;
+
+console.log("O Primeiro Número é maior que o Número 2 ?:",num1 > num2);
+console.log("O Primeiro Número é menor que o Número 2 ?:",num1 < num2);
+console.log("O Primeiro Número é maior ou igual que o Número 2 ?:",num1 >= num2);
+console.log("O Primeiro Número é menor ou igual que o Número 2 ?:",num1 <= num2);
+console.log("O Primeiro Número é diferente do Número 2 ?:",num1 != num2);
+console.log("O Primeiro Número é diferente em valor e tipo do Número 2 ?:",num1 !== num2);
+console.log("O Primeiro Número é parecido que o Número 2 ?:",num1 == num2);
+console.log("O Primeiro Número é identico que o Número 2 ?:",num1 === num2);
+*/
+
+let idade1= 8;
+let idade2 = 13;
+
+console.log("A Primeira Idade é maior que 10 e A Segunda Idade é menor que 12 ?:", idade1 > 10 && idade2 <12);
+console.log("A Primeira Idade é maior que 9 ou A Segunda Idade é menor que 15 ?:", idade1 > 9 || idade2 <=15);
+console.log("A Primeira Idade não é maior que 16 ?:", !(idade1 > 10) );
+console.log("A Segunda Idade não é menor que 11 ?:", !(idade2 < 11) );
