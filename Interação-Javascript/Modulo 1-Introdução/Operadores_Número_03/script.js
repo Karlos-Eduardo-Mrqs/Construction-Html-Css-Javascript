0e439e91ce349b1daf387f27b8d47385f3a225a1
Carlos Eduardo Marques Barreto <carloseduardomarquesbarretom@gmail.com>
2024-08-11 16:39:53 -0300

    Update script.js

---
+++ Interação-Javascript/Modulo 1-Introdução/Operadores_Número_03/script.js
@@ -0,0 +1,28 @@
+/*
+let num1 = 10;
+let num2 = 5;
+const Soma = num1 + num2; // Operador de Soma 
+const Subtracao = num1 - num2; // Operador de Subtração
+const Multiplicacao = num1 * num2; // Operador de Multiplicação
+const Divisao = num1/num2; // Operador de Divisão
+const Exponencial = num1 ** num2; // Operador de Exponencial 
+const Módulo = num1 % num2;// Operador de Resto da Divisão
+let incremento = num1 ++;// Incremento
+let decremento = num2 --; // Decremento
+
+console.log("Soma:",Soma);
+console.log("Subtração:",Subtracao);
+console.log("Multiplicação:",Multiplicacao);
+console.log("Divisão:",Divisao);
+console.log("Exponencial:",Exponencial);
+console.log("Resto Da Divisão:",Módulo);
+*/
+
+let num1 = 20;
+let num2 = 5;
+
+console.log("Soma :",num1 += num2);
+console.log("Subtração :",num1 -= num2);
+console.log("Multiplicação :",num1 *= num2);
+console.log("Divisão :",num1 /= num2);
+console.log("Resto De Divisão :",num1 %= num2);
